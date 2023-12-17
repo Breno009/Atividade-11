1a749d5e9649d76afe3243c904b4826be03b2fd4
Breno009 <147253894+Breno009@users.noreply.github.com>
2023-12-16 23:03:42 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,121 @@
+//var valor_1= prompt("Digite um número:")
+//var valor_2= prompt("Digite outro número:")
+//function mul(a, b) {
+  //  return a * b;
+//}
+//var texto = document.createTextNode(`Seu resultado é: ${mul(valor_1.value, valor_2.value)}`)
+let ç= prompt("Digite um número:")
+let v= prompt("Digite outro número:")
+function calcularProduto(a,b) {
+    return a*b
+}
+
+const r = calcularProduto(f,t)
+alert(r)
+
+
+let s= prompt("Digite o número de base")
+let b= prompt("Digite o número do expoente")
+function hg(a,b) {
+    return Math.pow (a,b)
+}
+const g = hg(h,b)
+alert(g)
+//alert(Math.pow(h,b))
+var n1= prompt("Digite seu saldo:")
+var n2= prompt("Digite o valor da sua compra:")
+function jjj(a,b) {
+    return a=>b
+}
+const x = jjj(n1,n2)
+if(x)
+//if(_n1=>n2){
+    alert(`A compra ocorreu corretamente! O seu saldo atual é de ${n1-n2}`)
+function ggg(a,b) {
+    return a<b
+}
+const h = ggg(n1,n2)
+ if(h){
+    //else if(h){
+    alert(`saldo insuficiente`)
+}
+let n4= prompt("Digite um número")
+let n5= prompt("Digite outro número")
+let n6= prompt("um caracter Digite um caracter (“+”, “-”, “*” ou “/”)")
+function kkk(a) {
+    return a=`*`
+}
+const t = kkk(n6)
+if (t){
+    alert(`${n4*n5}`)
+}
+function iii(a){
+    return a=`+`
+}
+const e = iii(n6)
+if (e){
+    alert(`${n4+n5}`)
+}
+function vvv(a){
+    return a=`-`
+}
+const y = vvv(n6)
+if (y){
+    alert(`${n4-n5}`)
+}
+function aav(a){
+    return a=`/`
+}
+const p = aav(n6)
+if (p){
+    alert(`${n4/n5}`)
+}
+let nt= Number(prompt("salário de um colaborador"))
+ op= nt * 0.20
+pl = nt * 0.15
+vur = nt * 0.10
+g = nt * 0.05
+function ççç(a) {
+    return a<=280
+}
+const z = ççç(nt)
+if (z){
+    alert(`Salário antes do reajuste ${nt} Salário depois do reajuste ${nt+op} valor do aumento ${op}`)
+}
+function qqq(a) {
+    return a>281 && a<700
+}
+const o = qqq(nt)
+if(o){
+    alert(`Salário antes do reajuste ${nt} Salário depois do reajuste ${nt+pl} valor do aumento ${pl}`)
+
+}
+function www(a) {
+    return a>700 && a<1500
+}
+const m = www(nt)
+if(m){
+    alert(`Salário antes do reajuste ${nt} Salário depois do reajuste ${nt+vur} valor do aumento ${vur}`)
+}
+function ccc(a) {
+   return a>=1501
+}
+const f = ccc(nt)
+if (f){
+    alert(`Salário antes do reajuste ${nt} Salário depois do reajuste ${nt+g} valor do aumento ${g}`)
+}
+let ol= prompt("Digite um lado de um triângulo:")
+let hj= prompt("Digite outro lado de um triângulo:")
+let jh= prompt("Digite outro lado de um triângulo:")
+if (hj+jhP || hj> ol+jh || jh> ol+hj ){
+    alert("Não é um triângulo")
+}
+else if(ol==hj==jh){
+    alert("Este triângulo é equilatero")
+}
+else if(ol==hj || ol==jh || hj==jh){
+    alert("Este triângulo é isósceles")
+}
+else(ol!==hj!==jh);{
+    alert("Este triângulo é Escaleno")
+}
